9f37c23f32be876ef2c542bcfdcfe6219eb2e77b
Clement <dapsen59@gmail.com>
2024-02-05 21:33:11 -0600

    Add files via upload

---
+++ sql_insert.py
@@ -0,0 +1,63 @@
+import os
+# import sys to get more detailed Python exception info
+import sys
+# import the connect library for psycopg2
+import psycopg2
+# import the error handling libraries for psycopg2
+from psycopg2 import OperationalError, errorcodes, errors
+import psycopg2.extras as extras
+import pandas as pd
+from io import StringIO
+import numpy as np
+from sqlalchemy import create_engine
+
+# Loading data from github
+irisData = pd.read_csv('https://raw.githubusercontent.com/Muhd-Shahid/Learn-Python-Data-Access/main/iris.csv',index_col=False)
+irisData.head()
+
+conn_params_dic = {
+    "host"      : "localhost",
+    "database"  : "irisdb",
+    "user"      : "backPASS?!1",
+    "password"  : "Passw0rd"
+}
+
+
+# Define a function that handles and parses psycopg2 exceptions
+def show_psycopg2_exception(err):
+    # get details about the exception
+    err_type, err_obj, traceback = sys.exc_info()    
+    # get the line number when exception occured
+    line_n = traceback.tb_lineno    
+    # print the connect() error
+    print ("\npsycopg2 ERROR:", err, "on line number:", line_n)
+    print ("psycopg2 traceback:", traceback, "-- type:", err_type) 
+    # psycopg2 extensions.Diagnostics object attribute
+    print ("\nextensions.Diagnostics:", err.diag)    
+    # print the pgcode and pgerror exceptions
+    print ("pgerror:", err.pgerror)
+    print ("pgcode:", err.pgcode, "\n")
+
+# Using alchemy method
+connect_alchemy = "postgresql+psycopg2://%s:%s@%s/%s" % (
+    conn_params_dic['user'],
+    conn_params_dic['password'],
+    conn_params_dic['host'],
+    conn_params_dic['database']
+)
+def using_alchemy(df):
+    try:
+        engine = create_engine(connect_alchemy)
+        df.to_sql('irisAlchemy', con=engine, index=False, if_exists='append',chunksize = 1000)
+        print("Data inserted using to_sql()(sqlalchemy) done successfully...")
+    except OperationalError as err:
+        # passing exception to function
+        show_psycopg2_exception(err)
+
+# Connect to the database
+engine = create_engine(connect_alchemy)
+# Importing data using_alchemy method
+using_alchemy(irisData)
+
+iris_df = pd.read_sql_table("irisAlchemy",con=engine)
+iris_df.head()
